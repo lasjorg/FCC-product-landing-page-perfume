9ab56fcadfbfb0a77580c4d72e6aa7a7c674b8ce
lasjorg <28780271+lasjorg@users.noreply.github.com>
2018-08-11 18:38:54 +0200

    Refactor nav

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
   const pageLoad = document.querySelector('.page-load');
   const burgerContainer = document.querySelector('.burger-container');
   const burger = document.querySelector('.burger-icon');
-  const mobileNavMenu = document.querySelector('.mobile-nav');
+  const navList = document.querySelector('.js-nav-trigger');
   let = lastScrollY = 0, ticking = false;
   
   // https://www.html5rocks.com/en/tutorials/speed/animations/#debouncing-scroll-events
@@ -54,10 +54,11 @@
   const triggerMobileNav = (e) => {
     // Toggle mobile menu on or off
     if ( (e.target === burger || e.target === burgerContainer) && !isToggled) {
-      mobileNavMenu.classList.add('show');
+      // navList.classList.add('show', 'mobile-nav');
+      navList.classList.add('show');
       isToggled = true;
     } else if (isToggled){
-      mobileNavMenu.classList.remove('show');
+      navList.classList.remove('show');
       isToggled = false;
     }
   }
